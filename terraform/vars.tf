9f50cb6faa87b32a38cb44721b4de32ddbc3f1af
ks <kskulikovaa@gmail.com>
2026-01-29 18:36:23 +0100

    upd: mark do_token as sensitive

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -1,5 +1,6 @@
 variable "do_token" {
     description = "Your DigitalOcean token"
+    sensitive = true
 }
 variable "public_key_name" {
     description = "Your public ssh key name on Digital Ocean"
